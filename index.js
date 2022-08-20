f73fe9297f2906a7a5717d7cdda99262a55edae9
raselASrock <mdraselm413@gmail.com>
2022-08-20 22:26:50 +0600

    if-else-added

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-console.log('hello');
-var name = 'hero Alom';
+// console.log('hello');
+var exampleType = null;
+console.log(typeof exampleType);
+
+var number1 = 71.25479863225;
+var number2 = 52.569855485494;
+var result = number1 + number2;
+console.log(parseFloat(result).toFixed(2))
+// console.log(parseInt (number));
+
+
+/*
+0 - 32 = F Grade ;
+33 - 39 = D Grade ;
+40 - 49 = C Grade ;
+50 - 59 = B Grade ;
+60 - 69 = A- Grade ;
+70 - 79 = A Grade ;
+80 - 95 = A+ Grade ;
+95 - 100 = Golden A+ Grade ;
+*/ 
+
+var result = 35 ;
+
+if ( result > 0 && result < 32 ) {
+    console.log("Sorry, You just failed in the exam");
+}
+
+else if( result >= 33 && result <40){
+    console.log('You score "D" Grade in this exam');
+}
