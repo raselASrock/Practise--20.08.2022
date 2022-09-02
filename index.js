f6e559e554336e5d99cad55e4bdda6abbbf85bf7
raselASrock <mdraselm413@gmail.com>
2022-09-02 10:46:42 +0600

    resourse-added

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // console.log('hello');
-var exampleType = null;
-console.log(typeof exampleType);
+// var exampleType = null;
+// console.log(typeof exampleType);
 
-var number1 = 71.25479863225;
-var number2 = 52.569855485494;
-var result = number1 + number2;
-console.log(parseFloat(result).toFixed(2))
+// var number1 = 71.25479863225;
+// var number2 = 52.569855485494;
+// var result = number1 + number2;
+// console.log(parseFloat(result).toFixed(2))
 // console.log(parseInt (number));
 
 
 /*
+Condition
 0 - 32 = F Grade ;
 33 - 39 = D Grade ;
 40 - 49 = C Grade ;
@@ -33,19 +34,44 @@ console.log(parseFloat(result).toFixed(2))
 // switch
 // Switch, Case, Break, Default
 
-var name = 'Hero'
+// var name = 'Hero'
 
-switch( name ){
-    case 'Rahim':
-        console.log("'Wow! It's Rahim");
-    break;
-    case 'Abdul':
-        console.log("Wow! It's Abdul Rahim Vai.");
-    break;
-    case 'Hero' :
-        console.log("Wow! Tumie amr sei 'Hero'");
-    break;
-    default :
-        console.log("Don't Know")
+// switch( name ){
+//     case 'Rahim':
+//         console.log("'Wow! It's Rahim");
+//     break;
+//     case 'Abdul':
+//         console.log("Wow! It's Abdul Rahim Vai.");
+//     break;
+//     case 'Hero' :
+//         console.log("Wow! Tumie amr sei 'Hero'");
+//     break;
+//     default :
+//         console.log("Don't Know")
     
+// }
+
+// Arrey
+
+// var numbers = [0, 1, 'Rahim', 3, 4, 5, true, 7, 8, 9];
+
+// numbers.push('Tumi')
+// numbers.pop()
+// numbers.shift();
+// numbers.unshift('Real')
+// numbers.join('-')
+
+// console.log(numbers);
+
+// Loop
+// for (let i = 0; i < 10 ; i++) {
+//     console.log(i) ;
+    
+// }
+
+var friends = ['Rahim', 'Karim', 'Tarim', 'Jamil', 'Sumit', 'Subin'];
+
+for( i = 0; i < friends.length; i++){
+    var element = friends[i];
+    console.log(element);
 }
